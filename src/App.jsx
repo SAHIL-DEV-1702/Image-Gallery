3521ad4e04f905e82da5797df73d4b6480aa3160
Sahil Patil <patilsahil@172002@gmail.com>
2025-12-31 12:15:58 +0530

    Pagination and lazy loading added

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ function App() {
 
   useEffect(() => {
     getData()
-  }, [page])
+  }, [page,])
 
   const pageInc = () => {
     setPage(page + 1)
@@ -37,34 +37,45 @@ function App() {
 
     }
   }
+  let printData = <h3 className="text-center text-blue-300 mt-90 text-6xl font-semibold">LOADING...</h3>
 
-  return (
-    <>
+  if (data.length > 0) {
+    return (
+      <>
 
-      <h1 className='h-10 mb-10 font-extrabold font-sans text-3xl text-cyan-300 text-center mt-5'> IMAGE GALLERY </h1>
+        <h1 className='h-10 mb-10 font-extrabold font-sans text-3xl text-cyan-300 text-center mt-5'> IMAGE GALLERY </h1>
 
-      <div className="body w-screen h-175 border-2 border-black flex flex-wrap  overflow-y-scroll justify-center">
+        <div className="body w-screen h-175 border-2 border-black flex flex-wrap  overflow-y-scroll justify-center">
 
-        {data.map((e, idx) =>
-          <a href={e.url} target="_blank" key={idx} >
-            <div className="card h-48 w-48 border-2 border-black m-5 rounded-xl overflow-hidden" >
-              <img src={e.download_url} alt="not found" className='w-full h-full' />
+          {data.map((e, idx) =>
+            <a href={e.url} target="_blank" key={idx} >
+              <div className="card h-48 w-48 border-2 border-black m-5 rounded-xl overflow-hidden" >
+                <img src={e.download_url} alt="not found" className='w-full h-full' />
 
-            </div>
-          </a>
-        )
-        }
+              </div>
+            </a>
+          )
+          }
 
 
-      </div>
-      <div className=" h-15 flex justify-center items-center mt-5 mb-5 space-x-5">
-        <button className='h-10 w-10 border-2 active:bg-gray-300' onClick={pageDec}>prev</button>
-        <button className='h-10 w-10 border-2 active:bg-gray-300' onClick={pageInc}>next</button>
+        </div>
+        <div className=" h-15 flex justify-center items-center mt-5 mb-5 space-x-5">
+          {page === 1 ?
+            <button className='h-10 w-15 border-2 bg-gray-100 rounded-lg font-bold' onClick={pageDec} disabled >prev</button>
+            :
+            <button className='h-10 w-15 border-2 bg-amber-200 active:scale-95 rounded-lg font-bold' onClick={pageDec} >prev</button>
+          }
+          <h4 className='font-bold'>Page {page}</h4>
+          <button className='h-10 w-15 border-2 bg-amber-200 active:scale-95 rounded-lg font-bold ' onClick={pageInc}>next</button>
 
-      </div>
-    </>
+        </div>
+      </>
 
-  )
+    )
+  }
+  else {
+    return printData
+  }
 }
 
 export default App
